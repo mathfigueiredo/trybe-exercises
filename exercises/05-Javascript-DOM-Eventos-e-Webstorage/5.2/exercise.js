d70a649871111d1adb85144dc8544d747ad4eb28
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-01 10:12:50 -0300

    task 2

--- exercises/05-Javascript-DOM-Eventos-e-Webstorage/5.2/exercise.js
+++ exercises/05-Javascript-DOM-Eventos-e-Webstorage/5.2/exercise.js
@@ -6,6 +6,9 @@ h1.innerText = 'Exercício 5.2 - Javascript DOM';
 document.body.appendChild(h1);
 
 // 2
+let divMainContent = document.createElement('div');
+divMainContent.className = 'main-content';
+document.body.appendChild(divMainContent);
 
 // 3
 
