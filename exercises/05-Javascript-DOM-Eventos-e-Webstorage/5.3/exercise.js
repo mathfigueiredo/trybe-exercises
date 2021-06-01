477e0c26b0ff6a304bcdd966b553f2ae9b33dddd
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-01 11:38:26 -0300

    task 1

---
+++ exercises/05-Javascript-DOM-Eventos-e-Webstorage/5.3/exercise.js
@@ -0,0 +1,56 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+  26, 27, 28, 29, 30, 31,
+];
+
+let dia = document.createElement('li');
+
+function fillDays() {
+  for (let i = 0; i < dezDaysList.length; i++) {
+    dia = document.createElement('li');
+    dia.classList.add('day');
+    dia.innerText = dezDaysList[i];
+    document.getElementById('days').appendChild(dia);
+  }
+}
+
+fillDays();
+
+for (let i = 0; i < document.querySelectorAll('.day').length; i++) {
+  if (
+    document.querySelectorAll('.day')[i].innerText === '24' ||
+    document.querySelectorAll('.day')[i].innerText === '25' ||
+    document.querySelectorAll('.day')[i].innerText === '31'
+  ) {
+    document.querySelectorAll('.day')[i].classList.add('holiday');
+  }
+}
+
+for (let i = 0; i < document.querySelectorAll('.day').length; i++) {
+  if (
+    document.querySelectorAll('.day')[i].innerText === '4' ||
+    document.querySelectorAll('.day')[i].innerText === '11' ||
+    document.querySelectorAll('.day')[i].innerText === '18' ||
+    document.querySelectorAll('.day')[i].innerText === '25'
+  ) {
+    document.querySelectorAll('.day')[i].classList.add('friday');
+  }
+}
+
