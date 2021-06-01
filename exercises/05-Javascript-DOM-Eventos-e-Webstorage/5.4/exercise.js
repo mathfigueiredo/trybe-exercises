8aaf5ca37f899f81144228eb3c31901b94e00e22
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-01 14:31:06 -0300

    adiciona margin left e width dos inputs

---
+++ exercises/05-Javascript-DOM-Eventos-e-Webstorage/5.4/exercise.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// margin-left: 2rem
+for (let i = 0; i < document.querySelectorAll('label').length; i++) {
+  document.querySelectorAll('label')[i].style.marginRight = '2rem';
+}
+
+// width: 40px
+for (let i = 0; i < document.querySelectorAll('.small').length; i++) {
+  document.querySelectorAll('.small')[i].style.width = '40px';
+}
+
