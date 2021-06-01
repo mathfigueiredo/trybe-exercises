4d00612d221f9f3a6b780566c48c40128234826b
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-01 10:08:03 -0300

    task 1

---
+++ exercises/05-Javascript-DOM-Eventos-e-Webstorage/5.2/exercise.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// 1
+let h1 = document.createElement('h1');
+h1.innerText = 'Exercício 5.2 - Javascript DOM';
+document.body.appendChild(h1);
+
+// 2
+
+// 3
+
+// 4
+
+// 5
+
+// 6
+
+// 7
+
+// 8
+
+// 9
