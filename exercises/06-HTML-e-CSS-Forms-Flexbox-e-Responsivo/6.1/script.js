a0cb6d1ccd314b7b204d71ae024ad18d9142368b
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-09 14:38:58 -0300

    adiciona os seletores em variaveis no js

--- exercises/06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/6.1/script.js
+++ exercises/06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/6.1/script.js
@@ -2,6 +2,20 @@
 
 const selectState = document.getElementById('input-state');
 
+const name = document.getElementById('input-name');
+const email = document.getElementById('input-email');
+const cpf = document.getElementById('input-cpf');
+const adress = document.getElementById('input-adress');
+const city = document.getElementById('input-city');
+const state = document.getElementById('input-state');
+const tipoCasa = document.getElementById('input-tipo-casa');
+const tipoApartamento = document.getElementById('input-tipo-apartamento');
+const resume = document.getElementById('input-resume');
+const jobPosition = document.getElementById('input-job-position');
+const jobDescription = document.getElementById('input-job-description');
+const startDate = document.getElementById('input-start-date');
+const btnSubmit = document.getElementById('btn-submit');
+
 const states = [
   'AC',
   'AL',
@@ -38,3 +52,8 @@ for (let i = 0; i < states.length; i++) {
   opt.innerText = states[i];
   selectState.appendChild(opt);
 }
+
+btnSubmit.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(name.value);
+});
