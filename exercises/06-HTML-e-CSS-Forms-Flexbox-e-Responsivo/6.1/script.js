be98fd8e0ee0561327014aacf4eeb6167e02e57f
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-09 14:41:17 -0300

    organiza js

--- exercises/06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/6.1/script.js
+++ exercises/06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/6.1/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const selectState = document.getElementById('input-state');
-
+// Selectors
 const name = document.getElementById('input-name');
 const email = document.getElementById('input-email');
 const cpf = document.getElementById('input-cpf');
@@ -16,6 +15,8 @@ const jobDescription = document.getElementById('input-job-description');
 const startDate = document.getElementById('input-start-date');
 const btnSubmit = document.getElementById('btn-submit');
 
+// States input
+const selectState = document.getElementById('input-state');
 const states = [
   'AC',
   'AL',
@@ -53,6 +54,9 @@ for (let i = 0; i < states.length; i++) {
   selectState.appendChild(opt);
 }
 
+// Functions
+
+// EventListeners
 btnSubmit.addEventListener('click', function (e) {
   e.preventDefault();
   console.log(name.value);
