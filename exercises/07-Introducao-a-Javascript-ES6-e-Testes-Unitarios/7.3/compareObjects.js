1888bf130ed5b4829c495cd63320acb695cfadaa
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-21 13:05:59 -0300

    adiciona comparaçao de objetos

---
+++ exercises/07-Introducao-a-Javascript-ES6-e-Testes-Unitarios/7.3/compareObjects.js
@@ -0,0 +1,20 @@
+const str = (x) => JSON.stringify(x);
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+// implemente seus testes aqui
+assert.strictEqual(str(obj1), str(obj2)); // false
