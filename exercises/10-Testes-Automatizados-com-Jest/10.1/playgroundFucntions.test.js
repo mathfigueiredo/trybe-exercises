7ae7a5e06cf77619a1c4b77ab507e51f95e17fb0
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 12:31:26 -0300

    Parte 2 - Teste 1.3: encode/decode

--- exercises/10-Testes-Automatizados-com-Jest/10.1/playgroundFucntions.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/playgroundFucntions.test.js
@@ -7,3 +7,8 @@ test('Verifica se encode é uma função', () => {
 test('Verifica se decode é uma função', () => {
   expect(typeof decode).toBe('function');
 });
+
+test('Verifica se, na função ENCODE, as vogais a,e,i,o,u são substituídas por 1,2,3,4,5', () => {
+  expect(encode('aeiou')).toBe('12345');
+});
+
