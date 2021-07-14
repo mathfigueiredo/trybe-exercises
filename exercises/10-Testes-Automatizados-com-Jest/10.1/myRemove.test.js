f9a3fc0326d41d08ce4c8ff4986e8a780a26eb0e
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 11:55:31 -0300

    Teste 2.2: myRemove

--- exercises/10-Testes-Automatizados-com-Jest/10.1/myRemove.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/myRemove.test.js
@@ -3,3 +3,7 @@ const myRemove = require('./myRemove');
 test('Remove 3 de [1, 2, 3, 4]', () => {
   expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
 });
+
+test('Remove 3 de [1, 2, 3, 4]', () => {
+  expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
