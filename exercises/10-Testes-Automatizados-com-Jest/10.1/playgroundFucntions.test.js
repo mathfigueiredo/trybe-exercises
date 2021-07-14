94b254c8ecc8dc3740e29c437a55895577606e54
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 12:34:27 -0300

    Parte 2 - Teste 1.4: encode/decode

--- exercises/10-Testes-Automatizados-com-Jest/10.1/playgroundFucntions.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/playgroundFucntions.test.js
@@ -15,3 +15,11 @@ test('Verifica se, na função ENCODE, as vogais a,e,i,o,u são substituídas po
 test('Verifica se, na função DECODE, os numerais 1,2,3,4,5 são substituídas por a,e,i,o,u', () => {
   expect(decode('12345')).toBe('aeiou');
 });
+
+test('Verifica se, na função ENCODE, Matheus é convertido para M1th25s', () => {
+  expect(encode('Matheus')).toBe('M1th25s');
+});
+
+test('Verifica se, na função DECODE, M1th25s é convertido para Matheus', () => {
+  expect(decode('M1th25s')).toBe('Matheus');
+});
