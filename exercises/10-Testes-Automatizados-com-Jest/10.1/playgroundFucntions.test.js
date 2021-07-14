538cd9f3571a7314bfde51da21ed8af9516c680e
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 12:38:53 -0300

    Parte 2 - Teste 1.5: encode/decode

--- exercises/10-Testes-Automatizados-com-Jest/10.1/playgroundFucntions.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/playgroundFucntions.test.js
@@ -23,3 +23,15 @@ test('Verifica se, na função ENCODE, Matheus é convertido para M1th25s', () =
 test('Verifica se, na função DECODE, M1th25s é convertido para Matheus', () => {
   expect(decode('M1th25s')).toBe('Matheus');
 });
+
+test('Verifica se, na função ENCODE, o tamanho da string de return tem o mesmo tamanho da entrada', () => {
+  const str = 'Matheus';
+  const len = str.length;
+  expect(encode(str).length).toBe(len);
+});
+
+test('Verifica se, na função DECODE, o tamanho da string de return tem o mesmo tamanho da entrada', () => {
+  const str = 'M1th25s';
+  const len = str.length;
+  expect(decode(str).length).toBe(len);
+});
