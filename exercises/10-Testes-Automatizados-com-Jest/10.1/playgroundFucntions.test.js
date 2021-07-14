1d35790f9a68fa0ef575372786579338da916f28
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 12:43:00 -0300

    Parte 2 - Teste 2: techList

--- exercises/10-Testes-Automatizados-com-Jest/10.1/playgroundFucntions.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/playgroundFucntions.test.js
@@ -1,5 +1,6 @@
 const { encode, decode, techList, hydrate } = require('./playgroundFunctions');
 
+// ENCODE / DECODE
 test('Verifica se encode é uma função', () => {
   expect(typeof encode).toBe('function');
 });
@@ -34,4 +35,43 @@ test('Verifica se, na função DECODE, o tamanho da string de return tem o mesmo
   const str = 'M1th25s';
   const len = str.length;
   expect(decode(str).length).toBe(len);
+});
+
+// TECHLIST
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
+  });
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
+  });
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(
+      techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')
+    ).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas',
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas',
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas',
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas',
+      },
+      {
+        tech: 'React',
+        name: 'Lucas',
+      },
+    ]);
+  });
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
+  });
 });
