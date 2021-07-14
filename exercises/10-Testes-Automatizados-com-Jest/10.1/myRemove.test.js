c86db78509037e0e83628b377f790416ee1a2f34
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 11:58:49 -0300

    Teste 2.3: myRemove

--- exercises/10-Testes-Automatizados-com-Jest/10.1/myRemove.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/myRemove.test.js
@@ -7,3 +7,9 @@ test('Remove 3 de [1, 2, 3, 4]', () => {
 test('Remove 3 de [1, 2, 3, 4]', () => {
   expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
 });
+
+test('Array passado como parâmetro não sofreu alterações', () => {
+  const arr = [1, 2, 3, 4];
+  myRemove(arr, 3);
+  expect(arr).toEqual([1, 2, 3, 4]);
+});
