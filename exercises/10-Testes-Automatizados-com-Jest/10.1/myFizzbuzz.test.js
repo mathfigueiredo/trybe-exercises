098ccdaffe9299b9342eb70309f1923bece2d1dc
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 12:10:16 -0300

    Teste 4.2: myFizzBuzz

--- exercises/10-Testes-Automatizados-com-Jest/10.1/myFizzbuzz.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/myFizzbuzz.test.js
@@ -3,3 +3,7 @@ const myFizzBuzz = require('./myFizzbuzz');
 test('Verifica se myFizzBuzz(15) retorna "fizzbuzz"', () => {
   expect(myFizzBuzz(15)).toBe('fizzbuzz');
 });
+
+test('Verifica se myFizzBuzz(9) retorna "fizz"', () => {
+  expect(myFizzBuzz(9)).toBe('fizz');
+});
