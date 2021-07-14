c3309b8ac9527a9641330eda6337efeb5d277116
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 12:18:06 -0300

    Teste 5.2: compareObjects

--- exercises/10-Testes-Automatizados-com-Jest/10.1/objectCompare.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/objectCompare.test.js
@@ -3,3 +3,11 @@ const { obj1, obj2, obj3 } = require('./objectCompare');
 test('Compara objetos iguais', () => {
   expect(obj1).toMatchObject(obj2);
 });
+
+test('Compara objetos iguais', () => {
+  expect(obj1).not.toMatchObject(obj3);
+});
+
+test('Compara objetos iguais', () => {
+  expect(obj2).not.toMatchObject(obj3);
+});
