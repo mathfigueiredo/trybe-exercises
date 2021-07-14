cb17ae8840ab4b682147a0f5c9ca76adbe2d0371
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-14 12:03:24 -0300

    Teste 3.2: myRemoveWithoutCopy

--- exercises/10-Testes-Automatizados-com-Jest/10.1/myRemobeWithoutCopy.test.js
+++ exercises/10-Testes-Automatizados-com-Jest/10.1/myRemobeWithoutCopy.test.js
@@ -3,3 +3,7 @@ const myRemoveWithoutCopy = require('./myRemobeWithoutCopy');
 test('Remove 3 de [1, 2, 3, 4]', () => {
   expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
 });
+
+test('Remove 3 de [1, 2, 3, 4]', () => {
+  expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
