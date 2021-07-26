c984f86b7b8f6421fa76a8bb878c06038c5b9689
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-26 13:37:24 -0300

    Componentizaçao

--- Frontend/12-Componentes-com-Estado-Eventos-e-Formularios-com-React/12.2/src/Form.js
+++ Frontend/12-Componentes-com-Estado-Eventos-e-Formularios-com-React/12.2/src/Form.js
@@ -1,61 +1,22 @@
 import React from 'react';
+import NameInput from './NameInput';
+import EmailInput from './EmailInput';
+import CpfInput from './CpfInput';
+import AddressInput from './AddressInput';
+import StateSelect from './StateSelect';
+import HouseType from './HouseType';
 
 class Form extends React.Component {
   render() {
     return (
       <form>
         <fieldset>
-          <label for="nameinput">Nome: </label>
-          <input
-            type="text"
-            maxLength="5"
-            id="nameinput"
-            name="nameinput"
-            required
-          />
-
-          <label for="emailinput">Email: </label>
-          <input
-            type="email"
-            maxLength="50"
-            id="emailinput"
-            name="emailinput"
-            required
-          />
-
-          <label for="cpfinput">CPF: </label>
-          <input
-            type="text"
-            maxLength="11"
-            id="cpfinput"
-            name="cpfinput"
-            required
-          />
-
-          <label for="addressinput">Endereço: </label>
-          <input
-            type="text"
-            maxLength="200"
-            id="addressinput"
-            name="addressinput"
-            required
-          />
-
-          <label for="stateselect">Estado: </label>
-          <select name="stateselect" id="stateselect">
-            <option value="Exemplo">Exemplo</option>
-          </select>
-
-          <label>Tipo: </label>
-          <input type="radio" id="house" name="housetype" value="house" />
-          <label for="house">Casa</label>
-          <input
-            type="radio"
-            id="appartment"
-            name="housetype"
-            value="appartment"
-          />
-          <label for="appartment">Apartamento</label>
+          <NameInput />
+          <EmailInput />
+          <CpfInput />
+          <AddressInput />
+          <StateSelect />
+          <HouseType />
         </fieldset>
       </form>
     );
