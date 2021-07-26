84670bbb55e34fe92468e26befa016327647dfc4
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-26 13:27:26 -0300

    Todos os campos do fieldset 1 - HTML only

--- Frontend/12-Componentes-com-Estado-Eventos-e-Formularios-com-React/12.2/src/Form.js
+++ Frontend/12-Componentes-com-Estado-Eventos-e-Formularios-com-React/12.2/src/Form.js
@@ -4,7 +4,59 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        <input />
+        <fieldset>
+          <label for="nameinput">Nome: </label>
+          <input
+            type="text"
+            maxLength="5"
+            id="nameinput"
+            name="nameinput"
+            required
+          />
+
+          <label for="emailinput">Email: </label>
+          <input
+            type="email"
+            maxLength="50"
+            id="emailinput"
+            name="emailinput"
+            required
+          />
+
+          <label for="cpfinput">CPF: </label>
+          <input
+            type="text"
+            maxLength="11"
+            id="cpfinput"
+            name="cpfinput"
+            required
+          />
+
+          <label for="addressinput">Endereço: </label>
+          <input
+            type="text"
+            maxLength="200"
+            id="addressinput"
+            name="addressinput"
+            required
+          />
+
+          <label for="stateselect">Estado: </label>
+          <select name="stateselect" id="stateselect">
+            <option value="Exemplo">Exemplo</option>
+          </select>
+
+          <label>Tipo: </label>
+          <input type="radio" id="house" name="housetype" value="house" />
+          <label for="house">Casa</label>
+          <input
+            type="radio"
+            id="appartment"
+            name="housetype"
+            value="appartment"
+          />
+          <label for="appartment">Apartamento</label>
+        </fieldset>
       </form>
     );
   }
